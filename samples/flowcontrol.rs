ed5a11b7259c719838fc5409fd514ee20e1614a5
Michael Greenly <mgreenly@gmail.com>
2020-12-11 13:38:04 -0600

    play with flow control

---
+++ samples/flowcontrol.rs
@@ -0,0 +1,20 @@
+// nothing to interesting here else is opitonal
+// the expression between the if and the opening bracket must evaluate to a bool
+
+fn main() {
+    let mut a: i32 = 7;
+    let b: i32;
+
+    if a > 2 {
+        b = 47;
+    } else {
+        b = 7;
+
+    }
+
+    if (a == 7) && true {
+     a = 1;
+    }
+
+    println!("a is {} and b is {}", a, b);
+}
